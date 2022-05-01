5811be109069e0a580b853c5e3e295c66ace3ce7
Ramzey <remzesii@gmail.com>
2022-05-01 09:30:42 +0500

    Сделано 1 задание

---
+++ ex001/Program.cs
@@ -0,0 +1,11 @@
+﻿// . Напишите программу вычисления модуля числа.
+// 2 -> 2
+// -3 -> 3
+// -7 -> 7
+Console.WriteLine("Введите любое целое число");
+
+string a = Console.ReadLine();
+int b = int.Parse(a);
+int c = Math.Abs(b);
+
+Console.WriteLine($"Модуль числа {a} равен {c}");
