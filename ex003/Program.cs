12c90577db5442b8d8ea5c3de9b49edd5bb21962
Ramzey <remzesii@gmail.com>
2022-05-01 10:14:06 +0500

    Сделано 3 задание

---
+++ ex003/Program.cs
@@ -0,0 +1,26 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+Console.WriteLine($"Введите первое число: ");
+
+string a = Console.ReadLine().Replace('.',',');
+
+double aa = Convert.ToDouble(a);
+
+Console.WriteLine($"Введите первое число: ");
+
+string b = Console.ReadLine().Replace('.',',');
+
+double bb = Convert.ToDouble(b);
+
+if (aa > bb){
+    Console.WriteLine($"Число {a} больше числа {b}");
+}
+    
+
+if (aa < bb){
+    Console.WriteLine($"Число {b} больше числа {a}");
+}
+    
+
+
