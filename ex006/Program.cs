ea45fa1fc9c24f2f7766ecf1990167863acb4c6d
Ramzey <remzesii@gmail.com>
2022-05-01 10:59:59 +0500

    Сделано 6 задание

---
+++ ex006/Program.cs
@@ -0,0 +1,19 @@
+﻿// 4. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите любое целое число: ");
+
+string a = Console.ReadLine().Replace('.',',');
+
+double b = Convert.ToDouble(a);
+
+int count = 2;
+
+Console.WriteLine($"Список четных чисел в промежутке от 1 до {b}: ");
+
+while(count <= b){
+    Console.Write($"{count}, ");
+    count = count + 2;
+}
+
+
